324ebc3a21c2b819c4976e3565b0ea752fbe8a17
Chiran Nuwan Bandara <chiran@surge.global>
2023-03-17 09:16:26 +0530

    undefined bug fix

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -2,12 +2,13 @@ import axios from 'axios';
 import Cookies from 'universal-cookie';
 
 const cookie = new Cookies()
+const token = cookie.get( "user" )?.token
 
 const api = axios.create( {
   baseURL: 'http://localhost:9000/api/',
   timeout: 1000,
   headers: {
-    Authorization: `Bearer ${cookie.get( "user" ).token}`
+    Authorization: `Bearer ${token}`
   }
 } )
 
